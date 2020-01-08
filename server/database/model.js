d7565d977b88e1e3b63d635d8cd5e580b8e06ac5
Robert Baker <zanotam@gmail.com>
2020-01-08 13:21:04 -0700

    CRUD API is fully armed and operational!

--- server/database/model.js
+++ server/database/model.js
@@ -1,27 +1,29 @@
-const Restaurant = require('./database/schema.js');
 const db = require('./index.js')
+const _ = require('underscore');
 
 //TO-DO: Update to Postgres
 const model = {
-  read: async function(id) {
-    return db.query(`SELECT * FROM restaurants WHERE id = ${id}`)
+
+  read: async function (id) {
+    return db.query(`SELECT * FROM restaurants WHERE id = ${id}`);
   },
 
-  create: async function(json) {
+  create: async function (id, json) {
     return db.query(`INSERT INTO restaurants VALUES
-    (${json.id}, '${_.escape(json.title)}', ARRAY ${JSON.stringify(json.articles)});`);
+    (${id}, '${_.escape(json.title)}', ARRAY ${JSON.stringify(json.articles)});`);
   },
+
   //I'm not entirely sure this will work as intended, but I think it will.
-  update: async function(id, articleId) {
-    return db.query(`SELECT articlesArray FROM restaurants WHERE id = ${id}`)
-      .then((results) => (db.query(`UPDATE restaurants SET articlesArray = ${JSON.stringify([results, articleId].flat())} WHERE id = ${id}`)
-
+  update: async function (id, articleId) {
+    return db.query(`SELECT articleArray FROM restaurants WHERE id = ${id}`)
+      .then((results) =>
+      (db.query(`UPDATE restaurants SET articleArray = ARRAY ${JSON.stringify([results.rows[0].articlearray, articleId].flat())} WHERE id = ${id}`)));
   },
 
-  delete: async function(id) {
+  delete: async function (id) {
     return db.query(`DELETE FROM restaurants WHERE id = ${id}`)
   }
 
 }
 
-export default model;
+module.exports = model;
